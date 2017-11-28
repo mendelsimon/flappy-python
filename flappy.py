23b7db90541a54a0ed03afd569e05ac2bb861484
Mendel Simon <mmendelsimon@gmail.com>
2017-11-27 22:04:25 -0500

    Added graphics

--- flappy.py
+++ flappy.py
@@ -7,10 +7,14 @@ FPS = 60
 
 WINDOW_HEIGHT = 700
 WINDOW_WIDTH = 500
+GROUND_HEIGHT = 112
+ground_offset = 0
+PLAY_AREA_HEIGHT = WINDOW_HEIGHT - GROUND_HEIGHT
 PLAYER_HEIGHT = 25
 PLAYER_WIDTH = 25
 # TODO calculate jump distance/frames to determine to pipe width (width = distance = frames*SPEED)
 PIPE_WIDTH = 70
+PIPE_HEAD_HEIGHT = 26
 
 SPEED = 2.5
 JUMP_SPEED = -7
@@ -18,7 +22,7 @@ GRAVITY = 0.4
 JUMP_HEIGHT = (JUMP_SPEED ** 2) / (GRAVITY * 2)
 
 # This area of the pipe will never have a gap
-PIPE_SOLID_AREA = WINDOW_HEIGHT / 5
+PIPE_SOLID_AREA = int(PLAY_AREA_HEIGHT / 5)
 
 current_frame = 0
 last_pipe_frame = 0  # The frame when the last pipe spawned
@@ -31,8 +35,9 @@ score = 0
 class Bird:
     def __init__(self):
         self.x = (WINDOW_WIDTH / 2) - (PLAYER_WIDTH / 2)
-        self.y = WINDOW_HEIGHT / 4
+        self.y = PLAY_AREA_HEIGHT / 4
         self.fall_speed = 0
+        self.image = pygame.image.load('python.png')
 
     def jump(self):
         self.fall_speed = JUMP_SPEED
@@ -46,19 +51,28 @@ class Pipe:
     def __init__(self, gap_height):
         self.x = WINDOW_WIDTH
         self.gap_height = gap_height
-        self.gap = random.randrange(PIPE_SOLID_AREA, WINDOW_HEIGHT - PIPE_SOLID_AREA - int(self.gap_height))
+        self.gap = random.randrange(PIPE_SOLID_AREA, PLAY_AREA_HEIGHT - PIPE_SOLID_AREA - int(self.gap_height))
         self.scored = False
+
+        head_image = pygame.image.load('pipe_head.png')
+        self.bottom_head_image = pygame.transform.scale(head_image, (PIPE_WIDTH, PIPE_HEAD_HEIGHT))
+        self.top_head_image = pygame.transform.flip(self.bottom_head_image, False, True)
+        self.body_image = pygame.image.load('pipe_body.png')
 
     def update_position(self):
         self.x -= SPEED
 
 
 def main():
-    global DISPLAY_SURFACE, FPSCLOCK
+    global DISPLAY_SURFACE, FPSCLOCK, BACKGROUND_IMAGE, GROUND_IMAGE
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAY_SURFACE = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Flappy Python")
+    BACKGROUND_IMAGE = pygame.image.load('background.png')
+    BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (WINDOW_WIDTH, PLAY_AREA_HEIGHT))
+    GROUND_IMAGE = pygame.image.load('ground.png')
+    GROUND_IMAGE = pygame.transform.scale(GROUND_IMAGE, (WINDOW_WIDTH, GROUND_HEIGHT))
 
     while True:
         play()
@@ -121,16 +135,16 @@ def spawn_pipe():
 
 
 def check_collision():
-    GRACE_LENGTH = 2  # The amount of collision that won't count as collision
+    GRACE_LENGTH = 5  # The amount of collision that won't count as collision
 
     # Check if player left window bounds
-    if player.y + GRACE_LENGTH < 0 or player.y - GRACE_LENGTH > WINDOW_HEIGHT:
+    if player.y + GRACE_LENGTH < 0 or player.y - GRACE_LENGTH > PLAY_AREA_HEIGHT:
         return True  # Game over
 
     # Check if player collided with a pipe
     for pipe in pipes:
         if pipe.x < player.x + PLAYER_WIDTH - GRACE_LENGTH and pipe.x + PIPE_WIDTH - GRACE_LENGTH > player.x:
-            if player.y < pipe.gap - GRACE_LENGTH or player.y > pipe.gap + pipe.gap_height + GRACE_LENGTH:
+            if player.y < pipe.gap - GRACE_LENGTH or player.y + PLAYER_HEIGHT > pipe.gap + pipe.gap_height + GRACE_LENGTH:
                 return True  # Game over
 
     return False  # No collision
@@ -162,22 +176,38 @@ def draw_screen():
 
 
 def draw_background():
-    DISPLAY_SURFACE.fill((50, 50, 50))
+    global ground_offset
+    DISPLAY_SURFACE.blit(BACKGROUND_IMAGE, (0, 0))
+    DISPLAY_SURFACE.blit(GROUND_IMAGE, (-ground_offset, PLAY_AREA_HEIGHT))
+    DISPLAY_SURFACE.blit(GROUND_IMAGE, (-ground_offset + WINDOW_WIDTH, PLAY_AREA_HEIGHT))
+    ground_offset += SPEED
+    ground_offset %= WINDOW_WIDTH
 
 
 def draw_player():
-    player_rect = (player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT)
-    pygame.draw.rect(DISPLAY_SURFACE, (255, 0, 0), player_rect)
+    player_image = pygame.transform.scale(player.image, (PLAYER_WIDTH, PLAYER_HEIGHT))
+    DISPLAY_SURFACE.blit(player_image, (player.x, player.y))
 
 
 def draw_pipes():
     for pipe in pipes:
-        pipe_top = (pipe.x, 0, PIPE_WIDTH, pipe.gap)
-        pipe_bottom_height = WINDOW_HEIGHT - pipe.gap + pipe.gap_height
-        pipe_bottom = (pipe.x, pipe.gap + pipe.gap_height, PIPE_WIDTH, pipe_bottom_height)
+        top_head_pos = (int(pipe.x), pipe.gap - PIPE_HEAD_HEIGHT)
+        bottom_head_pos = (int(pipe.x), pipe.gap + pipe.gap_height)
 
-        pygame.draw.rect(DISPLAY_SURFACE, (100, 250, 100), pipe_top)
-        pygame.draw.rect(DISPLAY_SURFACE, (100, 250, 100), pipe_bottom)
+        bottom_height = int(PLAY_AREA_HEIGHT - (pipe.gap + pipe.gap_height)) + 1
+
+        top_body_pos = (int(pipe.x), 0)
+        top_body_scale = (PIPE_WIDTH, pipe.gap - PIPE_HEAD_HEIGHT)
+        bottom_body_pos = (int(pipe.x), pipe.gap + int(pipe.gap_height) + PIPE_HEAD_HEIGHT)
+        bottom_body_scale = (PIPE_WIDTH, bottom_height - PIPE_HEAD_HEIGHT)
+
+        top_body_image = pygame.transform.scale(pipe.body_image, top_body_scale)
+        bottom_body_image = pygame.transform.scale(pipe.body_image, bottom_body_scale)
+
+        DISPLAY_SURFACE.blit(top_body_image, top_body_pos)
+        DISPLAY_SURFACE.blit(pipe.top_head_image, top_head_pos)
+        DISPLAY_SURFACE.blit(pipe.bottom_head_image, bottom_head_pos)
+        DISPLAY_SURFACE.blit(bottom_body_image, bottom_body_pos)
 
 
 def game_over():
